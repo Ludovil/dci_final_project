ed2606c5cbba2d9752427273e8cb59047de4c143
riccaralv <riccaralv@gmail.com>
2023-05-02 10:17:13 +0200

    create search-engine component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,42 @@
+import { useState } from 'react';
 import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import Maps from './components/Maps.jsx';
+import SearchBox2 from './components/SearchBox.jsx';
 
 function App() {
-	// states
-	//const [user, setUser] = useState(null);
-	const [marker, setMarker] = useState([]);
+  const [selectPosition, setSelectPosition] = useState(null);
 
-	const URL = 'http://localhost:3000';
-
-	// action
-	useEffect(() => {
-		axios
-			.get(`${URL}/users/6447fe383a1282652c316121`)
-			.then((res) => {
-				//console.log(res.data.data.geocode);
-				setMarker(res.data.data.geocode);
-			})
-			.catch((err) => console.log(err));
-	}, []);
-
-	console.log(marker);
-
-	// render
-	return (
-		<>
-			<h1>Map test</h1>
-			{/* <MapContainer center={[48.8566, 2.3522]} zoom={13}>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-				/>
-
-				<Marker position={marker}></Marker>
-			</MapContainer> */}
-		</>
-	);
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          width: '100vw',
+          height: '100vh',
+        }}
+      >
+        <div
+          style={{
+            width: '50vw',
+            height: '100vh ',
+          }}
+        >
+          <Maps selectPosition={selectPosition} />
+        </div>
+        <div
+          style={{
+            width: '50vw',
+          }}
+        >
+          <SearchBox2
+            selectPosition={selectPosition}
+            setSelectPosition={setSelectPosition}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
