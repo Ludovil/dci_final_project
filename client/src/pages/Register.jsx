870d7e54d4cb3df92bea3dcf7b36b3b7402275b7
Ludovic Saule <ludovic.saule@dci.education>
2023-05-03 15:40:15 +0200

    refactor Register.jsx and RegisterForm.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,84 @@
 import RegisterForm from '../components/RegisterForm.jsx';
+import { useState } from 'react';
+import axios from 'axios';
 
 function Register() {
-  return (
-    <div>
-      <h1>Register</h1>
-      <RegisterForm />
-    </div>
-  );
+	//states
+	const [formData, setFormData] = useState({
+		userName: '',
+		email: '',
+		password: '',
+		profile_image: '',
+		address: {
+			country: '',
+			city: '',
+			postcode: '',
+			street: '',
+			housenumber: '',
+		},
+	});
+
+	const [submitedInput, setSubmitedInput] = useState({
+		userName: null,
+		email: null,
+		password: null,
+		profile_image: null,
+		address: {
+			country: null,
+			city: null,
+			postcode: null,
+			street: null,
+			housenumber: null,
+		},
+	});
+
+	//actions
+	const onChangeHandler = (e) => {
+		const value = e.target.value;
+		setFormData({ ...formData, [e.target.name]: value });
+	};
+
+	const onSubmitHandler = (e) => {
+		e.preventDefault();
+		setSubmitedInput(formData);
+		// remise à zero
+		setFormData({
+			userName: '',
+			email: '',
+			password: '',
+			profile_image: '',
+			address: {
+				country: '',
+				city: '',
+				postcode: '',
+				street: '',
+				housenumber: '',
+			},
+		});
+		console.log('state:', formData);
+		axios
+			.post('http://localhost:3000/users', JSON.stringify(formData), {
+				headers: { 'Content-Type': 'application/json' },
+			})
+			.then((res) => {
+				if (res.data.success) {
+					console.log('success');
+				} else {
+					console.log(res.data.message);
+				}
+			});
+	};
+	return (
+		<div>
+			<h1>Register</h1>
+			<RegisterForm
+				onChangeHandler={onChangeHandler}
+				onSubmitHandler={onSubmitHandler}
+				submitedInput={submitedInput}
+				formData={formData}
+			/>
+		</div>
+	);
 }
 
 export default Register;
